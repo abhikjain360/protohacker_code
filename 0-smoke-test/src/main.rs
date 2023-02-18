af6c9afc99d7c2c4d4cc1c287c771b92d40b747f
Abhik Jain <abhik@abhikjain.xyz>
2023-02-18 15:16:53 +0100

    use tokio

--- 0-smoke-test/src/main.rs
+++ 0-smoke-test/src/main.rs
@@ -1,176 +1,101 @@
-use std::{
-    env,
-    io::{ErrorKind, Read, Write},
-    net::SocketAddr,
-    time::Duration,
-};
+use std::{env, io, net::SocketAddr};
 
-use ahash::RandomState;
-use mio::{
-    event::Event,
+use futures::{stream::FuturesUnordered, StreamExt};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
-    Events, Interest, Poll, Token,
 };
-use tracing::{error, info, instrument};
-
-const SERVER_TOKEN: Token = Token(0);
-const TIMEOUT: Duration = Duration::from_millis(100);
-
-type HashMap<K, V> = indexmap::IndexMap<K, V, ahash::RandomState>;
+use tracing::info;
 
 struct StreamHandler {
-    stream: TcpStream,
     addr: SocketAddr,
+    stream: TcpStream,
+    closed: bool,
     buf: Vec<u8>,
-    to_write: usize,
+    write_to: usize,
 }
 
 impl StreamHandler {
-    fn with_stream(stream: TcpStream, addr: SocketAddr) -> Self {
-        Self {
-            stream,
-            buf: vec![0; 1024],
-            to_write: 0,
-            addr,
-        }
-    }
-
-    #[instrument(skip(self, event))]
-    fn try_rw(&mut self, event: &Event) -> anyhow::Result<bool> {
-        let mut close = false;
-
-        if event.is_readable() {
-            close = self.try_read()?;
-        }
-
-        self.try_write()?;
-
-        Ok(close)
-    }
-
-    #[instrument(skip(self))]
-    fn try_read(&mut self) -> anyhow::Result<bool> {
+    async fn try_read(mut self) -> io::Result<Self> {
         let StreamHandler {
             stream,
             buf,
-            to_write,
+            write_to,
+            closed,
             ..
-        } = self;
-        let old_len = *to_write;
-        let mut close = false;
+        } = &mut self;
 
-        // read until we are blocked or an error occurs
-        loop {
-            match stream.read(&mut buf[*to_write..]) {
-                Err(e) => match e.kind() {
-                    ErrorKind::Interrupted => continue,
-                    ErrorKind::WouldBlock => break,
-                    _ => return Err(e.into()),
-                },
+        let bytes_read = stream.read(&mut buf[*write_to..]).await?;
+        *write_to += bytes_read;
+        *closed = bytes_read == 0;
 
-                Ok(n) => {
-                    *to_write += n;
-
-                    info!(to_write = to_write, n = n, bl = buf.len());
-
-                    if *to_write == buf.len() {
-                        buf.resize(*to_write + 1024, 0);
-                        info!("resized bl = {}", buf.len());
-                        continue;
-                    }
-
-                    if n == 0 {
-                        close = true;
-                        break;
-                    }
-                }
-            }
-        }
-
-        Ok(close || *to_write == old_len)
+        Ok(self)
     }
 
-    #[instrument(skip(self))]
-    fn try_write(&mut self) -> anyhow::Result<()> {
+    async fn try_write(mut self) -> io::Result<Self> {
         let StreamHandler {
             stream,
             buf,
-            to_write,
-            addr,
-        } = self;
+            write_to,
+            ..
+        } = &mut self;
 
-        if *to_write == 0 {
-            return Ok(());
-        }
+        stream.write_all(&buf[..*write_to]).await?;
+        *write_to = 0;
 
-        info!("addr = {addr:?} to_write = {to_write}");
-        match stream.write_all(&buf[..*to_write]) {
-            Ok(()) => *to_write = 0,
-            Err(e) if e.kind() != ErrorKind::WouldBlock => return Err(e.into()),
-            _ => {}
-        }
-
-        Ok(())
+        Ok(self)
     }
 }
 
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt().init();
+
     let mut args = env::args();
-    args.next().expect("no binary name provided");
-    let socke_addr = args.next().expect("no port number provided").parse()?;
+    args.next().expect("no binary name");
 
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
-        .init();
+    let addr: SocketAddr = args.next().expect("no bind addr provided").parse()?;
 
-    let poll = &mut Poll::new()?;
-    let events = &mut Events::with_capacity(1024);
+    let listener = TcpListener::bind(addr).await?;
 
-    let server = &mut TcpListener::bind(socke_addr)?;
-    poll.registry().register(
-        server,
-        SERVER_TOKEN,
-        Interest::READABLE | Interest::WRITABLE,
-    )?;
-
-    let connections = &mut HashMap::with_capacity_and_hasher(1024, RandomState::new());
-    let mut token_index = 1;
+    let read_futures = &mut FuturesUnordered::new();
+    let write_futures = &mut FuturesUnordered::new();
 
     loop {
-        poll.poll(events, Some(TIMEOUT))?;
+        let new_conn_fut = listener.accept();
+        let conn_read_fut = read_futures.next();
+        let conn_write_fut = write_futures.next();
+        tokio::select! {
+            res = new_conn_fut => {
+                let (stream, addr) = res?;
+                let handler = StreamHandler {
+                    addr,
+                    stream,
+                    closed: false,
+                    buf: vec![0; 4096],
+                    write_to: 0,
+                };
+                info!("accepted connection from {addr:?}");
+                read_futures.push(handler.try_read());
+            },
+            opt = conn_read_fut => {
+                let handler = match opt {
+                    None => continue,
+                    Some(res) => res?
+                };
 
-        for event in &*events {
-            match event.token() {
-                SERVER_TOKEN => loop {
-                    let (mut stream, addr) = match server.accept() {
-                        Err(e) if e.kind() == ErrorKind::WouldBlock => break,
-                        Err(e) => {
-                            error!("{e:?} => {e}");
-                            break;
-                        }
-                        Ok(v) => v,
-                    };
+                write_futures.push(handler.try_write());
+            },
+            opt = conn_write_fut => {
+                let handler = match opt {
+                    None => continue,
+                    Some(res) => res?
+                };
 
-                    let token = Token(token_index);
-                    token_index += 1;
-
-                    poll.registry()
-                        .register(&mut stream, token, Interest::READABLE)?;
-                    connections.insert(token, StreamHandler::with_stream(stream, addr));
-
-                    info!("accepted connection from {addr}");
-                },
-                token => {
-                    let handler = match connections.get_mut(&token) {
-                        Some(v) => v,
-                        // sporadic events can happen, can safely be ignored
-                        None => continue,
-                    };
-                    if handler.try_rw(event)? {
-                        poll.registry().deregister(&mut handler.stream)?;
-                        info!("disconnected from {}", handler.addr);
-                        connections.remove(&token);
-                    }
+                if !handler.closed {
+                    read_futures.push(handler.try_read());
+                } else {
+                    info!("connection closed with {}", handler.addr);
                 }
             }
         }
