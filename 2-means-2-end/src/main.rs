38236001df087e4f841a9c5f1fc57309a03251ce
Abhik Jain <abhik@abhikjain.xyz>
2023-02-19 14:45:18 +0100

    2: means to an end

---
+++ 2-means-2-end/src/main.rs
@@ -0,0 +1,91 @@
+use std::{env, net::SocketAddr};
+
+use futures::{stream::FuturesUnordered, StreamExt};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
+use tracing::{error, info};
+
+macro_rules! allow_eof {
+    ($expr:expr) => {
+        match $expr {
+            Ok(v) => v,
+            Err(e) if e.kind() == std::io::ErrorKind::UnexpectedEof => return Ok(()),
+            Err(e) => return Err(e.into()),
+        }
+    };
+}
+
+async fn handle_stream(mut stream: TcpStream, addr: SocketAddr, tree: sled::Tree) -> anyhow::Result<()> {
+    loop {
+        match allow_eof!(stream.read_u8().await) {
+            b'I' => handle_insert(&mut stream, &tree).await?,
+            b'Q' => handle_query(&mut stream, &tree).await?,
+            b => {
+                error!("invalid operation byte: {b}");
+                break;
+            }
+        }
+    }
+
+    info!("closing connection with {addr}");
+    Ok(())
+}
+
+async fn handle_insert(stream: &mut TcpStream, tree: &sled::Tree) -> anyhow::Result<()> {
+    let timestamp = stream.read_i32().await?;
+    let price = stream.read_i32().await?;
+    tree.insert(timestamp.to_be_bytes(), &price.to_be_bytes())?;
+    Ok(())
+}
+
+async fn handle_query(stream: &mut TcpStream, tree: &sled::Tree) -> anyhow::Result<()> {
+    let start = stream.read_i32().await?;
+    let end = stream.read_i32().await?;
+
+    let (len, sum) = tree
+        .range(start.to_be_bytes()..=end.to_be_bytes())
+        .try_fold((0, 0), |(len, sum), res| {
+            res.map(|(_, v)| (len + 1, sum + i32::from_be_bytes([v[0], v[1], v[2], v[3]])))
+        })?;
+    let avg = ((sum as f64) / (len as f64)).ceil() as i32;
+
+    stream.write_i32(avg).await?;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt().init();
+
+    let mut args = env::args();
+    args.next().expect("no binary name");
+
+    let addr: SocketAddr = args.next().expect("no addr").parse()?;
+
+    let server = TcpListener::bind(addr).await?;
+
+    let mut connections = FuturesUnordered::new();
+    let db = sled::open("clients").expect("unable to open db");
+
+    loop {
+        tokio::select! {
+            res = server.accept() => {
+                let (stream, addr) = res?;
+                info!("accepted connection from {addr}");
+                let tree = db.open_tree(addr.to_string().as_bytes())?;
+                connections.push(handle_stream(stream, addr, tree));
+            }
+            opt_res = connections.next() => {
+                if let Some(Err(e)) = opt_res {
+                    error!("{e}");
+                }
+            }
+        }
+    }
+
+    #[allow(unreachable_code)]
+    Ok(())
+}
