e02c024d7fcd3adb05fcf73b3ea6d87c04acb2fb
Abhik Jain <abhik@abhikjain.xyz>
2023-02-19 12:52:28 +0100

    use AsyncBufRead

--- 1-prime-time/src/main.rs
+++ 1-prime-time/src/main.rs
@@ -1,154 +1,27 @@
-use std::{env, net::SocketAddr};
+use std::{env, net::SocketAddr, time::Duration};
 
 use futures::{stream::FuturesUnordered, StreamExt};
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::{TcpListener, TcpStream},
 };
-use tracing::{debug, info, warn, Level};
+use tracing::{error, info, warn};
 
-struct ReadHandler {
-    addr: SocketAddr,
-    stream: TcpStream,
-    closed: bool,
-}
-
-impl ReadHandler {
-    async fn try_read(self) -> anyhow::Result<WriteHandler> {
-        let ReadHandler {
-            mut stream, addr, ..
-        } = self;
-
-        let mut read_bytes = 0;
-        let mut buf = vec![0; 4096];
-        let mut closed;
-
-        let input = loop {
-            let n = stream.read(&mut buf[read_bytes..]).await?;
-            closed = n == 0;
-            read_bytes += n;
-
-            let s = match std::str::from_utf8(&buf[..read_bytes]) {
-                Ok(v) => v,
-                Err(_) => break None,
-            };
-
-            match serde_json::from_str::<WebInput>(s) {
-                Ok(input) if !input.is_valid() => {
-                    debug!(input = s);
-                    warn!("invalid input method \"{}\"", input.method);
-                    break None;
-                }
-                Ok(input) => {
-                    debug!(input = s);
-
-                    let rounded = input.number.round();
-                    if rounded != input.number {
-                        break Some(-1);
-                    } else {
-                        break Some(rounded as i64);
-                    }
-                }
-
-                Err(_) if closed => {
-                    debug!(input = s);
-                    break None;
-                }
-                Err(e) if buf[read_bytes - 1] == b'\n' => {
-                    debug!(input = s);
-
-                    warn!("serde_json: {e}");
-                    break None;
-                }
-                _ => {}
-            }
-
-            if read_bytes == buf.len() {
-                buf.resize(read_bytes + 1024, 0);
-            }
-        };
-
-        Ok(WriteHandler {
-            addr,
-            stream,
-            input,
-            closed,
-        })
+fn is_prime(val: f64) -> bool {
+    let round = val.round();
+    if round != val {
+        return false;
     }
-}
+    let val = round as i64;
 
-struct WriteHandler {
-    addr: SocketAddr,
-    stream: TcpStream,
-    input: Option<i64>,
-    closed: bool,
-}
-
-impl WriteHandler {
-    async fn try_write(self) -> anyhow::Result<ReadHandler> {
-        let WriteHandler {
-            mut stream,
-            input,
-            addr,
-            closed,
-        } = self;
-
-        if let Some(input) = input {
-            let output = &WebOutput::new(check_prime(input));
-            let mut output_str = serde_json::to_string(output)?;
-            output_str.push('\n');
-
-            stream.write_all(output_str.as_bytes()).await?;
-        } else {
-            stream.write_all(b"gibberish\n").await?;
-        };
-
-        Ok(ReadHandler {
-            addr,
-            stream,
-            closed,
-        })
-    }
-}
-
-#[derive(Deserialize)]
-struct WebInput {
-    method: String,
-    number: f64,
-}
-
-impl WebInput {
-    fn is_valid(&self) -> bool {
-        self.method == "isPrime"
-    }
-}
-
-#[derive(Serialize)]
-struct WebOutput {
-    method: String,
-    prime: bool,
-}
-
-impl WebOutput {
-    fn new(is_prime: bool) -> Self {
-        Self {
-            method: "isPrime".to_string(),
-            prime: is_prime,
-        }
-    }
-}
-
-fn check_prime(val: i64) -> bool {
     if val < 2 {
         return false;
-    };
+    }
 
     if val == 2 {
         return true;
     }
-
-    info!("checking for {val}");
 
     for x in 2..=(val as f64).sqrt().ceil() as i64 {
         if val % x == 0 {
@@ -156,66 +29,73 @@ fn check_prime(val: i64) -> bool {
         }
     }
 
-    return true;
+    true
+}
+
+#[derive(Deserialize)]
+struct Input {
+    method: String,
+    number: f64,
+}
+
+async fn handle_stream(mut stream: TcpStream, addr: SocketAddr) -> anyhow::Result<()> {
+    let (reader, mut writer) = stream.split();
+    let mut split_reader = BufReader::new(reader).split(b'\n');
+
+    while let Some(segment) = split_reader.next_segment().await? {
+        let Input { method, number } = match serde_json::from_slice(&segment) {
+            Ok(v) => v,
+            Err(e) => {
+                warn!("serde_json: {e}");
+                writer.write_all(b"gibberish").await?;
+                break;
+            }
+        };
+
+        if method != "isPrime" {
+            warn!("invalid method \"{method}\"");
+            writer.write_all(b"gibberish\n").await?;
+            break;
+        }
+
+        let output = serde_json::json!({ "method": "isPrime", "prime": is_prime(number) });
+        let mut output_str = serde_json::to_string(&output)?;
+        output_str.push('\n');
+
+        writer.write_all(output_str.as_bytes()).await?;
+    }
+
+    info!("closing connection with {addr}");
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt()
-        .with_max_level(Level::DEBUG)
-        .init();
+    tracing_subscriber::fmt().init();
 
     let mut args = env::args();
     args.next().expect("no binary name");
 
-    let addr: SocketAddr = args.next().expect("no bind addr provided").parse()?;
+    let addr: SocketAddr = args.next().expect("no addr").parse()?;
 
-    let listener = TcpListener::bind(addr).await?;
-
-    let read_futures = &mut FuturesUnordered::new();
-    let write_futures = &mut FuturesUnordered::new();
+    let server = TcpListener::bind(addr).await?;
+    let mut futures = FuturesUnordered::new();
 
     loop {
-        let new_conn_fut = listener.accept();
-        let conn_read_fut = read_futures.next();
-        let conn_write_fut = write_futures.next();
         tokio::select! {
-            res = new_conn_fut => {
+            res = server.accept() => {
                 let (stream, addr) = res?;
-                let handler = ReadHandler {
-                    addr,
-                    stream,
-                    closed: false,
-                };
-                info!("accepted connection from {addr:?}");
-                read_futures.push(handler.try_read());
-            },
-            opt = conn_read_fut => {
-                let handler = match opt {
-                    None => continue,
-                    Some(res) => res?
-                };
-
-
-                if handler.closed {
-                    info!("connection closed with {}", handler.addr);
-                } else {
-                    write_futures.push(handler.try_write());
-                }
-            },
-            opt = conn_write_fut => {
-                let handler = match opt {
-                    None => continue,
-                    Some(res) => res?
-                };
-
-                if handler.closed {
-                    info!("connection closed with {}", handler.addr);
-                } else {
-                    read_futures.push(handler.try_read());
+                info!("accepted connection from {addr}");
+                futures.push(handle_stream(stream, addr));
+            }
+            opt_res = futures.next() => {
+                if let Some(Err(e)) = opt_res {
+                    error!("{e}");
                 }
             }
         }
+
+        tokio::time::sleep(Duration::from_millis(100)).await;
     }
 
     #[allow(unreachable_code)]
