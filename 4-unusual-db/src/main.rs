85ecbe1d1402a1a3046d802303954789ba443d85
Abhik Jain <abhik@abhikjain.xyz>
2023-02-19 20:46:24 +0100

    4: unusual db

---
+++ 4-unusual-db/src/main.rs
@@ -0,0 +1,41 @@
+use std::{env, net::SocketAddr};
+
+use tokio::net::UdpSocket;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt().init();
+
+    let addr: SocketAddr = env::args().nth(1).expect("no addr").parse()?;
+
+    let socket = UdpSocket::bind(addr).await?;
+    let buf = &mut vec![0; 1000];
+
+    let db = sled::open("db")?;
+
+    loop {
+        let (bytes_read, addr) = socket.recv_from(buf).await?;
+        let mut data = buf[..bytes_read].splitn(2, |b| *b == b'=');
+
+        let key = data.next().expect("there should atleast be an empty slice");
+
+        match data.next() {
+            None => {
+                let value = db.get(key)?.unwrap_or(b"".into());
+
+                buf[bytes_read] = b'=';
+                let start = bytes_read + 1;
+                let end = start + value.len();
+                buf[start..end].copy_from_slice(&value);
+
+                socket.send_to(&buf[..end], addr).await?;
+            }
+            Some(value) => {
+                db.insert(key, value)?;
+            }
+        }
+    }
+
+    #[allow(unreachable_code)]
+    Ok(())
+}
