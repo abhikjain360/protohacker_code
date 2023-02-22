180ea9881923bc56575dd45df6436d89733fef17
Abhik Jain <abhik@abhikjain.xyz>
2023-02-22 19:26:33 +0100

    event loop in util

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,6 +1,8 @@
-use std::{env, net::SocketAddr};
+use std::{env, future::Future, net::SocketAddr};
 
 use anyhow::anyhow;
+use tokio::net::{TcpListener, TcpStream};
+use tracing::{error, info};
 
 #[macro_export]
 macro_rules! log_and_exit {
@@ -30,4 +32,39 @@ pub fn init_tracing() {
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .init()
+}
+
+pub async fn accept_loop<F, Fut, State>(f: F, addr: SocketAddr, state: State) -> anyhow::Result<()>
+where
+    Fut: Future<Output = anyhow::Result<()>> + Send,
+    F: FnOnce(TcpStream, State) -> Fut + Copy + Sync + Send + 'static,
+    State: Send + Clone + 'static,
+{
+    let listener = TcpListener::bind(addr).await?;
+
+    loop {
+        let (stream, addr) = listener.accept().await?;
+        info!("accepted connection from {addr}");
+
+        let state = state.clone();
+        tokio::spawn(async move {
+            if let Err(e) = f(stream, state).await {
+                error!("{e}");
+            }
+            info!("closing connection with {addr}");
+        });
+    }
+
+    #[allow(unreachable_code)]
+    Ok(())
+}
+
+pub async fn accept_loop_with_env<F, Fut, State>(f: F, state: State) -> anyhow::Result<()>
+where
+    Fut: Future<Output = anyhow::Result<()>> + Send,
+    F: FnOnce(TcpStream, State) -> Fut + Copy + Sync + Send + 'static,
+    State: Send + Clone + 'static,
+{
+    init_tracing();
+    accept_loop(f, addr_from_args()?, state).await
 }
