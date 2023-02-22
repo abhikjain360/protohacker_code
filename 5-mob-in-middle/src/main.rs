be60a723b34dd5962e741b17e4ccc7a4e3f668b9
Abhik Jain <abhik@abhikjain.xyz>
2023-02-22 10:08:36 +0100

    recompile

--- 5-mob-in-middle/src/main.rs
+++ 5-mob-in-middle/src/main.rs
@@ -28,14 +28,14 @@ fn replace_wallet(message: &[u8]) -> Vec<u8> {
     let mut i = 0;
 
     while i < message.len() {
-        let Some(pos) = message[i..].iter().position(|b| !b.is_ascii_whitespace()) else {
+        let Some(pos) = message[i..].iter().position(|b| *b != b' ') else {
             res.extend_from_slice(&message[i..]);
             break;
         };
         let start = i + pos;
         res.extend_from_slice(&message[i..start]);
 
-        let end = match message[start..].iter().position(u8::is_ascii_whitespace) {
+        let end = match message[start..].iter().position(|b| *b == b' ') {
             Some(pos) => start + pos,
             None => message.len(),
         };
