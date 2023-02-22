75417eb5d37d8cf9208d9177f9e656d7544eccac
Abhik Jain <abhik@abhikjain.xyz>
2023-02-22 09:42:20 +0100

    handle invalid client stuff

--- 5-mob-in-middle/src/main.rs
+++ 5-mob-in-middle/src/main.rs
@@ -16,32 +16,50 @@ macro_rules! log_and_exit {
 const UPSTREAM_ADDR: &str = "chat.protohackers.com:16963";
 const TONY_WALLET: &[u8] = b"7YWHMfk9JZe0LM0g1ZauHuiSxhI";
 
-fn is_boguscoin(segment: &[u8]) -> bool {
-    segment.len() >= 26 && segment.len() <= 35 && segment[0] == b'7'
+fn is_wallet_addr(segment: &[u8]) -> bool {
+    segment.len() >= 26
+        && segment.len() <= 35
+        && segment[0] == b'7'
+        && segment.iter().all(u8::is_ascii_alphanumeric)
 }
 
 fn replace_wallet(message: &[u8]) -> Vec<u8> {
-    let mut result = message
-        .split(|b| *b == b' ')
-        .map(|segment| {
-            if is_boguscoin(segment) {
-                return TONY_WALLET;
-            }
-            segment
-        })
-        .fold(vec![], |mut v, segment| {
-            v.extend(segment);
-            v.push(b' ');
-            v
-        });
-    result.pop();
-    result
+    let mut res = Vec::with_capacity(message.len());
+    let mut i = 0;
+
+    while i < message.len() {
+        let Some(pos) = message[i..].iter().position(|b| !b.is_ascii_whitespace()) else {
+            res.extend_from_slice(&message[i..]);
+            break;
+        };
+        let start = i + pos;
+        res.extend_from_slice(&message[i..start]);
+
+        let end = match message[start..].iter().position(u8::is_ascii_whitespace) {
+            Some(pos) => start + pos,
+            None => message.len(),
+        };
+
+        let segment = &message[start..end];
+
+        if is_wallet_addr(segment) {
+            res.extend_from_slice(TONY_WALLET);
+        } else {
+            res.extend_from_slice(segment);
+        }
+
+        i = end;
+    }
+
+    #[allow(unreachable_code)]
+    res
 }
 
 async fn handle_stream(mut stream: TcpStream, addr: SocketAddr) -> anyhow::Result<()> {
     let mut upstream = TcpStream::connect(UPSTREAM_ADDR).await?;
     let (upstream_reader, mut upstream_writer) = upstream.split();
-    let mut upstream_lines = BufReader::new(upstream_reader).split(b'\n');
+    let upstream_buf = &mut Vec::new();
+    let mut upstream_lines = BufReader::new(upstream_reader);
 
     let (client_reader, mut client_writer) = stream.split();
     let client_buf = &mut Vec::new();
@@ -57,13 +75,13 @@ async fn handle_stream(mut stream: TcpStream, addr: SocketAddr) -> anyhow::Resul
                 upstream_writer.write_all(&replace_wallet(&client_buf[..n])).await?;
                 client_buf.clear();
             }
-            upstream_msg_res = upstream_lines.next_segment() => {
-                let mut msg = match upstream_msg_res? {
-                    Some(upstream_msg) => replace_wallet(&upstream_msg),
-                    None => break,
-                };
-                msg.push(b'\n');
-                client_writer.write_all(&msg).await?;
+            res = upstream_lines.read_until(b'\n', upstream_buf) => {
+                let n = res?;
+                if n == 0 {
+                    break;
+                }
+                client_writer.write_all(&replace_wallet(&upstream_buf[..n])).await?;
+                upstream_buf.clear();
             }
         }
     }
@@ -73,7 +91,9 @@ async fn handle_stream(mut stream: TcpStream, addr: SocketAddr) -> anyhow::Resul
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt().init();
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
 
     let mut args = env::args();
     args.next().expect("no binary name");
